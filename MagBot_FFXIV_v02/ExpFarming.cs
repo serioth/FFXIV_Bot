a92df1048c7666321d73c6c5dd7ee8c62ec9ad85
serioth <anders.orchid@gmail.com>
2014-10-24 20:21:10 +0100

    Naming

--- MagBot_FFXIV_v02/ExpFarming.cs
+++ MagBot_FFXIV_v02/ExpFarming.cs
@@ -8,7 +8,7 @@ namespace MagBot_FFXIV_v02
 {
     internal class ExpFarming
     {
-        //test
+        //next version branch
         private readonly Player _player;
         private readonly int _targetBaseOffset;
         private readonly int _targetTwoBaseOffset;
